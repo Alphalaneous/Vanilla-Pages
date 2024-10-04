e48fc2e1c675866c56793b82d0a0018370e27013
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2024-10-04 09:31:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vanilla Pages
+
+An mod to add pages to MenuLayer, CreatorLayer, Pause Layer, LevelInfoLayer, Editor Build Tabs, and Quick Search 
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <Geode/Geode.hpp>
+#include "layers/CreatorLayer.h"
+#include "layers/MenuLayer.h"
+#include "layers/PauseLayer.h"
+#include "layers/LevelInfoLayer.h"
+#include "layers/EditorUI.h"
